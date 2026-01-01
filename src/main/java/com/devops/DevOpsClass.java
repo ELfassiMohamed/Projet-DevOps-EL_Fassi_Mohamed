0a35d10075881b03f463be9dda754d992957c4a1
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-01-01 19:39:11 +0100

    2éme commit dans la branche Dev

--- src/main/java/com/devops/DevOpsClass.java
+++ src/main/java/com/devops/DevOpsClass.java
@@ -6,5 +6,8 @@ public class DevOpsClass {
 		
 		System.out.println("Bonjour et bon courage dans votre projet en DevOps");
 	}
-
+	
+	public String getMessage() {
+        return "Bonjour DevOps c'est la branche Dev , J'attends GitHub Action ;)";
+    }
 }
